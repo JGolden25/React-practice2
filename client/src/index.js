db551a27463cb7bea4e29cdd8da97a1f9527dc66
Joseph Chretien-Golden <joechre@gmail.com>
2020-02-06 17:53:48 -0600

    adds header,footer, css, fonts and some routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './Resources/css/styles.css';
 
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <Routes />
+    </BrowserRouter>
+, document.getElementById('root'));
 
 
